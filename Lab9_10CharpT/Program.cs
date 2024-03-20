1565be3e1ff95122631941621e312654ad333d52
user <user@WIN-MHGL1PRC8MH>
2024-03-20 23:07:39 +0200

    1

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
 
 // See https://aka.ms/new-console-template for more information
@@ -20,32 +24,61 @@ static void Main(string[] args){
     case 4: { four();  break;}
   }
 
-
+}
 static void one(){
 
 string file = File.ReadAllText("1.txt");
 
-Stack st = new Stack();
+Stack<int> st = new Stack<int>();
+Stack<char> st1 = new Stack<char>();
+Stack<int> res = new Stack<int>();
+
+
+foreach (char i in file){
+    if (char.IsDigit(i)){
+        st.Push(i-'0');
+    }
+
+    else if (i == 'M' || i == 'm'){
+        st1.Push(i);
+    }
+}
+
+if(st1.Count > 0){
+while (st.Count > 0){
+    int n = st.Pop();
+    int n1 = st.Pop();
+    char m = st1.Pop();
+
+    if(m=='M'){
+        res.Push(Math.Max(n, n1));
+    }
+    else{
+        res.Push(Math.Min(n, n1));
+    }
+}
+if (st.Count == 0){
+    int n = res.ElementAt(0);
+    int n1 = res.ElementAt(1);
+    char m = st1.Pop();
+
+    if(m=='M'){
+        res.Push(Math.Max(n, n1));
+    }
+    else{
+        res.Push(Math.Min(n, n1));
+    }
+}
+}
  
- for(int i = 0;i<4;i++){
-    int n = Convert.ToInt32(Console.ReadLine());
-    st.Push(n);
- }   
-
-
-Console.WriteLine("Stack: ");
-foreach(var elem in st)
-        {
-            Console.WriteLine(elem);
-        }
-
-Console.WriteLine("Result: ");
-
+ foreach (int i in res){
+    Console.WriteLine(i);
+ }
 
 Console.WriteLine("\n");
 
 }
-}
+
 static void two(){
     var people = new Queue<string[]>();
     int[] years= new int[3];
@@ -127,8 +160,6 @@ static void three(){
         years[i]=int.Parse(st[3]);
     }
 
-
-
     for(int i = 0;i<3;i++){
         if(years[i] < 30){
             a.Add(i);
@@ -146,19 +177,70 @@ static void three(){
 int k=0;
 
 
-foreach(var j in str )
+
 foreach(int m in a ){
-    for(int i = 0;i<3;i++){
+    foreach(var j in str ){
         if(k==m){
-            Console.WriteLine(j[i]);   
+            Console.WriteLine(j);   
         }
         k++;
     }
     k=0;
     }    
 
+    foreach(int m in a1 ){
+    foreach(var j in str ){
+        if(k==m){
+            Console.WriteLine(j);   
+        }
+        k++;
+    }
+    k=0;
+    }    
+
+
+/*
+    a.GetEnumerator().MoveNext();
+    th t = new th();
+    t.GetEnumerator();
+    th t1=(th)t.Clone();
+    t.GetEnumerator();
+    t.CompareTo(t1);
+
+*/
+
 Console.WriteLine("\n");
 
+}
+
+public class th : IEnumerable, IComparable, ICloneable{
+    public ArrayList st;
+    public ArrayList st1;
+
+    public th()
+        {
+            st = new ArrayList();
+            st1 = new ArrayList();
+        }
+    public IEnumerator GetEnumerator()
+        {
+            return st.GetEnumerator();
+        }
+
+    public int CompareTo(object obj)
+        {
+            int s =st.Count;
+            if(s!=0) {return s.CompareTo(st.Count);}
+        else{
+                throw new ArgumentException("Error");
+            }
+        }
+
+    public object Clone()
+        {
+            th t = new th();
+            return t;
+        }
 }
 
 static void four(){
@@ -170,6 +252,28 @@ double[] numb=new double[n];
 
 }
 
+public class musician{
+    List<string> disk = new List<string>();
+    
+    
+    public void add_disk(){
+        Console.WriteLine("Name of Disk: ");
+        disk.Add(Console.ReadLine());
+    }
+
+    public void delite_disk(){
+        Console.WriteLine("Delite ");
+        disk.RemoveAt(-1);
+    }
+
+    public void show(){
+        foreach (string i in disk){
+            Console.WriteLine(i);
+        }
+    }
+
+
+}
 
 
 }}
